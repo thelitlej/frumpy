22e730f5d3d7ee6ef3f69e8e512c32d6e0942e03
thelitlej <jonasgustafson@hotmail.se>
2018-10-01 16:56:56 +0200

    frog heading, drag strength and swipe angle is now implemented

--- frumpy/GameScene.swift
+++ frumpy/GameScene.swift
@@ -12,28 +12,54 @@ import GameplayKit
 class GameScene: SKScene {
   var startX: CGFloat = 0;
   var startY: CGFloat = 0;
-  var endX: CGFloat = 0;
-  var endY: CGFloat = 0;
-  var distance: CGFloat = 0;
   override func didMove(to view: SKView) {
     let panner = UIPanGestureRecognizer(target: self, action: #selector(GameScene.jumpSwipe(sender:)))
-
     view.addGestureRecognizer(panner)
   }
   @objc func jumpSwipe(sender: UIPanGestureRecognizer) {
     if(sender.state.rawValue == 1) {
       startX = sender.location(in: view).x
       startY = sender.location(in: view).y
+      print("START")
+      print("")
     } else if (sender.state.rawValue == 3) {
-      endX = sender.location(in: view).x
-      endY = sender.location(in: view).y
-      distance = sqrt(pow((startX - endX), 2) + pow((startY - endY), 2))
-      if(distance > 100) {
-        distance = 100;
-      }
-      print("distance: ", distance)
+      print("END")
+      print("Swipe Angle: ", calculateAngle(sender: sender))
+      print("Frog Heading:", calculateFrogHeading(swipeAngle: calculateAngle(sender: sender)))
+      print("Distance: ", calculateDistance(sender: sender))
+      print("")
+      //TODO: Implement frog jump
     }
-    
-   
+    else {
+      print("CHANGE")
+      print("Swipe Angle: ", calculateAngle(sender: sender))
+      print("Frog Heading:", calculateFrogHeading(swipeAngle: calculateAngle(sender: sender)))
+      print("Distance: ", calculateDistance(sender: sender))
+      print("")
+      //TODO: Implement heading dots
+    }
+  }
+  
+  @objc func calculateDistance(sender: UIPanGestureRecognizer) -> CGFloat {
+    let x = sender.location(in: view).x
+    let y = sender.location(in: view).y
+    var distance = sqrt(pow((x - startX) , 2) + pow((y - startY), 2))
+    if(distance > 150) {
+      distance = 150;
+    }
+    return distance
+  }
+  
+  @objc func calculateAngle(sender: UIPanGestureRecognizer) -> CGFloat {
+    let x = sender.location(in: view).x
+    let y = sender.location(in: view).y
+    return atan2((y - startY), (x - startX))
+  }
+  
+  @objc func calculateFrogHeading(swipeAngle: CGFloat) -> CGFloat {
+    if(swipeAngle < 0) {
+      return swipeAngle + CGFloat.pi
+    }
+    return swipeAngle - CGFloat.pi
   }
 }
