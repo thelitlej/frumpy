670f53ec4d155147ce66845389b9175f6d592b89
thelitlej <jonasgustafson@hotmail.se>
2018-10-01 14:55:44 +0200

    swipe function created

--- frumpy/GameScene.swift
+++ frumpy/GameScene.swift
@@ -1,28 +1,39 @@
+//
+//  GameScene.swift
+//  frumpy
+//
+//  Created by Jonas Gustafson on 2018-10-01.
+//  Copyright © 2018 Jonas Gustafson. All rights reserved.
+//
+
 import SpriteKit
+import GameplayKit
 
 class GameScene: SKScene {
-  let label = SKLabelNode(text: "Hello SpriteKit!")
-  
+  var startX: CGFloat = 0;
+  var startY: CGFloat = 0;
+  var endX: CGFloat = 0;
+  var endY: CGFloat = 0;
+  var distance: CGFloat = 0;
   override func didMove(to view: SKView) {
-    label.position = CGPoint(x: view.frame.width / 2, y: view.frame.height / 2)
-    label.fontSize = 45
-    label.fontColor = SKColor.lightGray
-    label.fontName = "Avenir"
-    addChild(label)
-    let recognizer = UITapGestureRecognizer(target: self, action: #selector(tap))
-    view.addGestureRecognizer(recognizer)
+    let panner = UIPanGestureRecognizer(target: self, action: #selector(GameScene.jumpSwipe(sender:)))
+
+    view.addGestureRecognizer(panner)
   }
-  
-  @objc func tap(recognizer: UIGestureRecognizer) {
-    let viewLocation = recognizer.location(in: view)
-    let sceneLocation = convertPoint(fromView: viewLocation)
-    let moveByAction = SKAction.moveBy(x: sceneLocation.x - label.position.x, y: sceneLocation.y - label.position.y, duration: 1)
+  @objc func jumpSwipe(sender: UIPanGestureRecognizer) {
+    if(sender.state.rawValue == 1) {
+      startX = sender.location(in: view).x
+      startY = sender.location(in: view).y
+    } else if (sender.state.rawValue == 3) {
+      endX = sender.location(in: view).x
+      endY = sender.location(in: view).y
+      distance = sqrt(pow((startX - endX), 2) + pow((startY - endY), 2))
+      if(distance > 100) {
+        distance = 100;
+      }
+      print("distance: ", distance)
+    }
     
-    // Reversed action
-    let moveByReversedAction = moveByAction.reversed()
-    let moveByActions = [moveByAction, moveByReversedAction]
-    let moveSequence = SKAction.sequence(moveByActions)
-    
-    label.run(moveSequence)
+   
   }
 }
