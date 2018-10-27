b245a766ceb4099d84d3652a84dd301ee9bbafcb
thelitlej <jonasgustafson@hotmail.se>
2018-10-27 15:53:29 +0200

    started with highscore and added appicon

--- frumpy/Scenes/LeaderboardScene.swift
+++ frumpy/Scenes/LeaderboardScene.swift
@@ -12,5 +12,15 @@ import GameplayKit
 class LeaderboardScene: SKScene {
   override func didMove(to view: SKView) {
     addChild(BackButton(position: CGPoint(x: 0 + 9.5 + 10, y: size.height - 15 - 10)))
+    backgroundColor = Utilities.UIColorFromHex(hexValue: 0x702D2D)
+    addHeroImage()
+  }
+  
+  private func addHeroImage() {
+    let texture = SKTexture(imageNamed: "highscorehero")
+    let spriteHero = SKSpriteNode(texture: texture)
+    spriteHero.size = CGSize(width: size.width - 25, height: 180)
+    spriteHero.position = CGPoint(x: size.width/2, y: size.height - spriteHero.size.height/2 - 50)
+    addChild(spriteHero)
   }
 }
